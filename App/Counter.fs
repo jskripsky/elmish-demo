7b512ca3fd2ae7c22e6edd08aa0eafed2c4d6b03
Juraj Skripsky <juraj.skripsky@gmail.com>
2024-04-13 16:59:47 +0200

    Cleanup.

--- App/Counter.fs
+++ App/Counter.fs
@@ -3,31 +3,30 @@ module Elmish.Demo.Counter
 open Feliz
 open Feliz.DaisyUI
 
-// The state of our app
-// (type with multiple fields)
+/// The state of our app (type with multiple fields)
 type State = {
    Counter: int
    Name: string
 }
 
-// A message triggered by UI
-// (type with multiple alternatives)
+/// A message triggered by UI (type with multiple alternatives)
 type Msg =
     | Increment
     | Decrement
     | ChangeName of string
 
-// Create the initial state
+/// Create the initial state.
 let init (): State =
    { Counter = 0; Name = "" }
 
-// Update the state according to the given message
+/// Update the state according to the given message.
 let update (msg: Msg) (s: State): State =
     match msg with
     | Increment -> { s with Counter = s.Counter + 1 }
     | Decrement -> { s with Counter = s.Counter - 1 }
     | ChangeName name -> { s with Name = name }
 
+/// Render the given state.
 let render (s: State) (dispatch: Msg -> unit): ReactElement =
     let button (label: string) msg =
         Daisy.button.button [
@@ -59,13 +58,16 @@ let render (s: State) (dispatch: Msg -> unit): ReactElement =
                 Daisy.input [
                     prop.className "input-bordered"
                     prop.type' "text"
+                    prop.placeholder "Name"
                     prop.valueOrDefault s.Name
                     prop.onTextChange (ChangeName >> dispatch)
                 ]
 
+                Html.hr []
+
                 for _ in [1..s.Counter] do
                     Daisy.badge [
-                        prop.className "badge-secondary"
+                        prop.className "badge-lg badge-secondary"
                         prop.text $"Hello {s.Name}"
                     ]
             ]
